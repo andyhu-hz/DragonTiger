ad38d04599912b12d1f8ecc7787dcb85bce3b9e1
Andy Hu <andy.d.hu@ringcentral.com>
2021-11-03 00:52:27 +0800

    added poker transition effect and fix bug

--- DragonTiger/DragonTiger/ViewController.m
+++ DragonTiger/DragonTiger/ViewController.m
@@ -22,13 +22,18 @@ uint32_t bet_hu = 0;
 uint32_t bet_he = 0;
 uint32_t bet_total = 0;
 
+bool in_round = true;
+
 uint32_t poker[8][4][13] = {0};
 uint8_t timer_count = 0;
+uint8_t point_long = 0;
 
 uint32_t myMoney = 1000;
 uint8_t click_count = 0;
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSTimer * mytimer;
 
 @end
 
@@ -43,7 +48,7 @@ uint8_t click_count = 0;
 -(void)tapDetected{
     ++ click_count;
     if(100 == click_count && myMoney < 100) {
-        AudioServicesPlaySystemSound (1520);
+        AudioServicesPlaySystemSound (1262);
         click_count = 0;
         myMoney = 1000;
         bet_long = 0;
@@ -51,10 +56,9 @@ uint8_t click_count = 0;
         bet_he = 0;
         ((UILabel *)[self.view viewWithTag:11]).text = [NSString stringWithFormat: @"%d", myMoney];
     } else {
-        AudioServicesPlaySystemSound (1521);
+        AudioServicesPlaySystemSound (1103);
     }
 }
-
 
 -(void)Init {
     for(uint8_t i=0; i<8; i++) {
@@ -87,7 +91,7 @@ uint8_t click_count = 0;
     [((UIImageView *)[self.view viewWithTag:200])  addGestureRecognizer:singleTap];
   
     
-    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(onTimer) userInfo:nil repeats:YES];
+    self.mytimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(onTimer) userInfo:nil repeats:YES];
     [self ReadPersonData];
     ((UILabel *)[self.view viewWithTag:11]).text = [NSString stringWithFormat: @"%d", myMoney];
     [(UIButton *)[self.view viewWithTag:1] setBackgroundColor:[UIColor greenColor]];
@@ -110,33 +114,36 @@ uint8_t click_count = 0;
     }
 }
 
--(void)RoundEnd {
+-(void)ShowPokerLong {
+    in_round = false;
     uint8_t i = arc4random_uniform(8);
     uint8_t j = arc4random_uniform(4);
     uint8_t k = arc4random_uniform(13);
-    uint32_t poker_long = poker[i][j][k];
-    uint8_t point_long = poker_long % 100;
-
-    uint8_t ii = arc4random_uniform(8);
-    uint8_t jj = arc4random_uniform(4);
-    uint8_t kk = arc4random_uniform(13);
     
-    while(i == ii && j == jj && k == kk) {
-        ii = arc4random_uniform(8);
-        jj = arc4random_uniform(4);
-        kk = arc4random_uniform(13);
-    }
-
-    uint32_t poker_hu = poker[ii][jj][kk];
-    uint8_t point_hu = poker_hu % 100;
-
+    uint32_t poker_long = poker[i][j][k];
+    point_long = poker_long % 100;
+    
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionReveal;
+    [((UIImageView *)[self.view viewWithTag:177]).layer addAnimation:transition forKey:nil];
     
     NSString *path = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%i",poker_long] ofType:@"png"];
     UIImage *image = [UIImage imageWithContentsOfFile:path];
     [((UIImageView *)[self.view viewWithTag:177]) setImage:image];
+}
+
+-(void)RoundEnd {
+    uint8_t i = arc4random_uniform(8);
+    uint8_t j = arc4random_uniform(4);
+    uint8_t k = arc4random_uniform(13);
+    uint32_t poker_hu = poker[i][j][k];
+    uint8_t point_hu = poker_hu % 100;
     
-    path = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%i",poker_hu] ofType:@"png"];
-    image = [UIImage imageWithContentsOfFile:path];
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionReveal;
+    [((UIImageView *)[self.view viewWithTag:188]).layer addAnimation:transition forKey:nil];
+    NSString *path = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%i",poker_hu] ofType:@"png"];
+    UIImage *image = [UIImage imageWithContentsOfFile:path];
     [((UIImageView *)[self.view viewWithTag:188]) setImage:image];
     
     int32_t outcome = 0;
@@ -176,18 +183,37 @@ uint8_t click_count = 0;
     } else {
         ((UILabel *)[self.view viewWithTag:11]).text = [NSString stringWithFormat: @"%d 破产了", myMoney];
     }
-    [self SavePersonData];
-    AudioServicesPlaySystemSound(1100);
+    AudioServicesPlaySystemSound (1103);
 }
 
 -(void) onTimer {
     timer_count ++;
-    if(10 < timer_count) {
-        timer_count = 0;
-        [self RoundEnd];
+    [self SavePersonData];
+    if(4 < timer_count) {
+        [(UIProgressView *)[self.view viewWithTag:10] setProgress:((float)(timer_count-5)/10.0) animated:YES];
     }
-    [(UIProgressView *)[self.view viewWithTag:10] setProgress:((float)timer_count/10.0) animated:YES];
-    //NSLog(@"UIProgressView : timer_count = %d", timer_count);
+    if(5 == timer_count) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"back" ofType:@"png"];
+        UIImage *image = [UIImage imageWithContentsOfFile:path];
+        CATransition *transition = [CATransition animation];
+        transition.type = kCATransitionReveal;
+        [((UIImageView *)[self.view viewWithTag:177]).layer addAnimation:transition forKey:nil];
+        [((UIImageView *)[self.view viewWithTag:188]).layer addAnimation:transition forKey:nil];
+        [((UIImageView *)[self.view viewWithTag:177]) setImage:image];
+        [((UIImageView *)[self.view viewWithTag:188]) setImage:image];
+        AudioServicesPlaySystemSound (1103);
+        in_round = true;
+    }
+
+    if(13 == timer_count) {
+        [self ShowPokerLong];
+    }
+    
+    if(15 == timer_count) {
+        [self RoundEnd];
+        timer_count = 0;
+    }
+    [self SavePersonData];
 }
 
 - (IBAction)IsDragonSelected:(id)obj {
@@ -226,23 +252,34 @@ uint8_t click_count = 0;
         ((UILabel *)[self.view viewWithTag:77]).text = [NSString stringWithFormat: @"%d", bet_hu];
         ((UILabel *)[self.view viewWithTag:88]).text = [NSString stringWithFormat: @"%d", bet_he];
         ((UILabel *)[self.view viewWithTag:11]).text = [NSString stringWithFormat: @"%d", myMoney];
+        AudioServicesPlaySystemSound (1103);
     }
 }
 
 - (IBAction)add100:(id)obj {
-    [self UpdateBet:100];
+    if(in_round) {
+        [self UpdateBet:100];
+    }
 }
 - (IBAction)add1k:(id)obj {
-    [self UpdateBet:1000];
+    if(in_round) {
+        [self UpdateBet:1000];
+    }
 }
 - (IBAction)add10k:(id)obj {
-    [self UpdateBet:10000];
+    if(in_round) {
+        [self UpdateBet:10000];
+    }
 }
 - (IBAction)add100k:(id)obj {
-    [self UpdateBet:100000];
+    if(in_round) {
+        [self UpdateBet:100000];
+    }
 }
 - (IBAction)add1m:(id)obj {
-    [self UpdateBet:1000000];
+    if(in_round) {
+        [self UpdateBet:1000000];
+    }
 }
 
 @end
